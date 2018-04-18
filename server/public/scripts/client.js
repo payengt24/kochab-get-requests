f04834442db21b6840c4c04d29724024dd7919ba
Pa Yeng Thao <payengt24@gmail.com>
2018-04-18 16:23:05 -0500

    finished

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,30 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jquery is loaded');
+    $.ajax({
+        type: 'GET',
+        url: '/all-quotes'
+    })
+    .then(function(response){
+        console.log(response);
+        $('#allQuotes').append('<li>' + response[0].quote + '</li>');
+        $('#allQuotes').append('<li>' + response[1].quote + '</li>');
+        $('#allQuotes').append('<li>' + response[2].quote + '</li>');
+        
+        
+    });
+
+    $('#quoteButton').on('click', function (){
+        console.log('button click');
+        $.ajax({
+            type: 'GET',
+            url: '/quotes'
+        })
+        .then(function(response){
+            console.log(response);
+            $('#randomQuotes').text(response.quote);
+        });
+        
+    });
+
 }
--- server/server.js
+++ server/server.js
@@ -5,19 +5,20 @@ const PORT = 5000;
 app.use(express.static('server/public'));
 
 app.get('/all-quotes', (req, res) => {
-    const quotes_data = [
+    const quotes = [
         { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
         { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
         { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
     ]; 
-});
-
-app.get('/all-quotes', (req, res) => {
-    res.send(quotes_data);
-
+    res.send(quotes);
 });
 
 app.get('/quotes', (req, res) => {
+    const quotes = [
+        { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+        { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+        { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+    ]; 
     const randomNumber = Math.floor(Math.random() * 3);
     res.send(quotes[randomNumber]);
 });
@@ -26,8 +27,6 @@ app.get('/', (req, res) => {
     console.log('yep, it works!');
     res.send('Happy Wednesday');
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
