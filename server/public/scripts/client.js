571afe5b1c7c6212b98b5acb39bbc258544d2616
Pa Yeng Thao <payengt24@gmail.com>
2018-04-19 21:34:50 -0500

    add function and input for new array

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,20 +4,12 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jquery is loaded');
-    $.ajax({
-        type: 'GET',
-        url: '/all-quotes'
-    })
-    .then(function(response){
-        console.log(response);
-        $('#allQuotes').append('<li>' + response[0].quote + '</li>');
-        $('#allQuotes').append('<li>' + response[1].quote + '</li>');
-        $('#allQuotes').append('<li>' + response[2].quote + '</li>');
-        
-        
-    });
+    getAllQuotes();
+    
+    $('#addNewQuoteButton').on('click', addNewQuote)
 
     $('#quoteButton').on('click', function (){
+        addNewQuote();
         console.log('button click');
         $.ajax({
             type: 'GET',
@@ -30,4 +22,39 @@ function onReady() {
         
     });
 
+}
+
+function getAllQuotes () {
+    $.ajax({
+        type: 'GET',
+        url: '/all-quotes'
+    })
+    .then(function(response){
+        console.log('my response', response);
+        $('#listOfQuotes').empty();
+        response.forEach(function (loopQuotes) {
+            console.log(loopQuotes);
+            $('#listOfQuotes').append('<li>' + loopQuotes.quote + '</li>')
+            
+        });
+    });
+}
+
+function addNewQuote (){
+    console.log('add new quote');
+    const newQuote = {
+       quote: $('#newQuote').val(),
+       author: $('#newQuoteAuthor').val()
+    };
+    console.log('New author: ', newQuote);
+    $.ajax({
+        method: 'POST',
+        url: '/add-quotes',
+        data: newQuote //the object
+    })
+        .then(function (response) {
+            console.log('my repose: ', response);
+            // $('#allQuotes').append('<li>' + newQuote + '</li>');
+            getAllQuotes ();
+        });
 }
--- server/server.js
+++ server/server.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 5000;
+
+const quotes = require('./modules/all-quotes');
 
 app.use(express.static('server/public'));
 
+app.use(bodyParser.urlencoded( { extended: true } ))
+
 app.get('/all-quotes', (req, res) => {
-    const quotes = [
-        { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
-        { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
-        { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
-    ]; 
     res.send(quotes);
 });
 
 app.get('/quotes', (req, res) => {
-    const quotes = [
-        { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
-        { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
-        { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
-    ]; 
     const randomNumber = Math.floor(Math.random() * 3);
     res.send(quotes[randomNumber]);
 });
+
+app.post('/add-quotes', (req, res) => {
+    console.log(req.body);
+    quotes.push(req.body);
+    res.sendStatus(200);
+})
 
 app.get('/', (req, res) => {
     console.log('yep, it works!');
